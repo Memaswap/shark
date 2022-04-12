621515ccb3866062f861f9ba29a9b6d6d799408c
Achigyus <gyuseachi@yahoo.com>
2022-04-12 15:57:58 +0100

    first commit

---
+++ src/components/Menu/config/config.ts
@@ -0,0 +1,172 @@
+import {
+  MenuItemsType,
+  DropdownMenuItemType,
+  SwapIcon,
+  SwapFillIcon,
+  EarnFillIcon,
+  EarnIcon,
+  TrophyIcon,
+  TrophyFillIcon,
+  NftIcon,
+  NftFillIcon,
+  MoreIcon,
+} from '@pancakeswap/uikit'
+import { ContextApi } from 'contexts/Localization/types'
+/* import { nftsBaseUrl } from 'views/Nft/market/constants'
+
+const perpetualLangs = { en: 'en', 'zh-cn': 'zh-CN', ko: 'ko' }
+const perpLangMap = (code: string) => {
+  if (perpetualLangs[code]) {
+    return perpetualLangs[code]
+  }
+  return 'en'
+} */
+
+export type ConfigMenuItemsType = MenuItemsType & { hideSubNav?: boolean }
+
+const config: (t: ContextApi['t'], code: string) => ConfigMenuItemsType[] = (t, code) => [
+  {
+    label: t('Exchange'),
+    icon: SwapIcon,
+    fillIcon: SwapFillIcon,
+    href: '/swap',
+    showItemsOnMobile: false,
+    items: [
+      {
+        label: t('Swap'),
+        href: '/swap',
+      } /* 
+      {
+        label: t('Limit'),
+        href: '/limit-orders',
+      }, */,
+      /* {
+        label: t('Zap'),
+        href: 'https://zap.sharknado.io',
+      }, */
+      {
+        label: t('Liquidity'),
+        href: '/liquidity',
+      } /* 
+      {
+        label: t('Perpetual'),
+        href: `https://perp.pancakeswap.finance/${perpLangMap(code)}/futures/BTCUSDT`,
+        type: DropdownMenuItemType.EXTERNAL_LINK,
+      }, */,
+    ],
+  }, 
+  {
+    label: t('Earn'),
+    href: '/farms',
+    icon: EarnIcon,
+    fillIcon: EarnFillIcon,
+    items: [
+      {
+        label: t('Staking'),
+        href: '/pools',
+      },
+      {
+        label: t('Farming'),
+        href: '/farms',
+      },
+    ],
+  },
+  // {
+  //   label: t('Zap'),
+  //   href: `https://zap.sharknado.io`,
+  //   icon: NftIcon,
+  //   fillIcon: NftFillIcon,
+  //   items: [],
+  // },
+  // {
+  //   label: t('Earn'),
+  //   href: '#',
+  //   icon: TrophyIcon,
+  //   fillIcon: TrophyFillIcon,
+  //   items: [
+  //     {
+  //       label: t('Staking'),
+  //       href: '/pools',
+  //     },
+  //     {
+  //       label: t('Farming'),
+  //       href: '#',
+  //     },
+  //     /* 
+  //     {
+  //       label: t('Trading Competition'),
+  //       href: '/competition',
+  //     },
+  //     {
+  //       label: t('Prediction (BETA)'),
+  //       href: '/prediction',
+  //     },
+  //     {
+  //       label: t('Lottery'),
+  //       href: '/lottery',
+  //     }, */
+  //   ],
+  // },
+  {
+    label: t('NFT'),
+    href: `#`,
+    icon: NftIcon,
+    fillIcon: NftFillIcon,
+    items: [
+      /* {
+        label: t('Overview'),
+        href: `${nftsBaseUrl}`,
+      },
+      {
+        label: t('Collections'),
+        href: `${nftsBaseUrl}/collections`,
+      },
+      {
+        label: t('Activity'),
+        href: `${nftsBaseUrl}/activity`,
+      }, */
+    ],
+  },
+  {
+    label: '',
+    href: '#',
+    icon: MoreIcon,
+    hideSubNav: true,
+    items: [
+      // {
+      //   label: t('SHARKO Analytics'),
+      //   href: 'https://pancakeswap.finance/info/token/0x7e1d65ee360335fa119ee4b9708e47700efbfc58',
+      // } /* 
+      // {
+      //   label: t('IFO'),
+      //   href: '/ifo',
+      // }, */,
+      // {
+      //   label: t('DAO'),
+      //   href: 'https://vote.sharknado.io',
+      // },
+      // {
+      //   type: DropdownMenuItemType.DIVIDER,
+      // },
+      {
+        label: t('Landing'),
+        href: '#',
+      },
+      {
+        type: DropdownMenuItemType.DIVIDER,
+      },
+      {
+        label: t('Blog'),
+        href: 'https://medium.com',
+        type: DropdownMenuItemType.EXTERNAL_LINK,
+      },
+      {
+        label: t('Docs'),
+        href: '#',
+        type: DropdownMenuItemType.EXTERNAL_LINK,
+      },
+    ],
+  },
+]
+
+export default config
---
+++ src/config/constants/pools.tsx
@@ -0,0 +1,65 @@
+import Trans from 'components/Trans'
+import { VaultKey } from 'state/types'
+import { CHAIN_ID } from './networks'
+import tokens, { serializeTokens } from './tokens'
+import { SerializedPoolConfig, PoolCategory } from './types'
+
+const serializedTokens = serializeTokens()
+
+export const vaultPoolConfig = {
+  [VaultKey.CakeVault]: {
+    name: <Trans>Auto CAKE</Trans>,
+    description: <Trans>Automatic restaking</Trans>,
+    autoCompoundFrequency: 5000,
+    gasLimit: 380000,
+    tokenImage: {
+      primarySrc: `/images/tokens/${tokens.cake.address}.svg`,
+      secondarySrc: '/images/tokens/autorenew.svg',
+    },
+  },
+  [VaultKey.IfoPool]: {
+    name: 'IFO CAKE',
+    description: <Trans>Stake CAKE to participate in IFOs</Trans>,
+    autoCompoundFrequency: 1,
+    gasLimit: 500000,
+    tokenImage: {
+      primarySrc: `/images/tokens/${tokens.cake.address}.svg`,
+      secondarySrc: `/images/tokens/ifo-pool-icon.svg`,
+    },
+  },
+} as const
+
+const pools: SerializedPoolConfig[] = [
+  ,
+  
+  {
+    sousId: 1,
+    stakingToken: serializedTokens.met,
+    earningToken: serializedTokens.met,
+    contractAddress: {
+      97: '0xB8F8812584B4441C529cc8a284907D4c5A04ba83',
+      56: '0x96479D99fb9f2C9585787174B4b1cAB5BD36eBeE',
+    },
+    poolCategory: PoolCategory.CORE,
+    harvest: true,
+    tokenPerBlock: '5',
+    sortOrder: 0,
+    isFinished: false,
+  }, 
+   {
+    sousId: 0,
+    stakingToken: serializedTokens.cake,
+    earningToken: serializedTokens.cake,
+    contractAddress: {
+      97: '0x1d32c2945C8FDCBc7156c553B7cEa4325a17f4f9',
+      56: '0x73feaa1eE314F8c655E354234017bE2193C9E24E',
+    },
+    poolCategory: PoolCategory.CORE,
+    harvest: true,
+    tokenPerBlock: '10',
+    sortOrder: 1,
+    isFinished: false,
+  },
+].filter((p) => !!p.contractAddress[CHAIN_ID])
+
+export default pools
