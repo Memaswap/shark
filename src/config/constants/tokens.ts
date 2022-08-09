9745df65273114e24699ce4c8fa0fc96c5bd8f27
Memaswap <110889639+Memaswap@users.noreply.github.com>
2022-08-09 09:32:54 +0100

    Update tokens.ts

--- src/config/constants/tokens.ts
+++ src/config/constants/tokens.ts
@@ -36,7 +36,7 @@ export const mainnetTokens = defineTokens({
     18,
     'MET',
     'MemaSwap Token',
-    'https://memaswap.com/',
+    'https://memaswap.org/',
   ),
   gmi: new Token(MAINNET, '0x93D8d25E3C9A847a5Da79F79ecaC89461FEcA846', 18, 'GMI', 'Gamifi', 'https://gamifi.gg/'),
   tlos: new Token(MAINNET, '0xb6C53431608E626AC81a9776ac3e999c5556717c', 18, 'TLOS', 'Telos', 'https://www.telos.net/'),
